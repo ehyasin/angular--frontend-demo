b895eb5e2bffa355404cfa331d0a89bbfe46a960
Ehtisham <ehtihsam@xlr8solutions.com>
2021-02-12 23:22:56 +0000

    tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,20 +16,18 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should have router-outlet', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    expect(fixture.nativeElement.querySelector('router-outlet')).not.toBeNull();
   });
 
   it(`should have as title 'application-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('application-frontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('application-frontend app is running!');
   });
 });
